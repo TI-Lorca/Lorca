68de3f6bdc8764d60d91318b410ba5e9ccee0f2e
gabiten <gabiten@gmail.com>
2024-12-11 10:33:44 -0300

    111112

--- js/custom.js
+++ js/custom.js
@@ -31,3 +31,23 @@ if($('.navbar').length > 0){
     });
 }
 
+
+// Función para alternar entre modo oscuro y claro
+function toggleTheme() {
+    const themeIcon = document.getElementById('theme-icon');
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        themeIcon.classList.remove('moon-stars-fill'); // Remover icono de luna
+        themeIcon.classList.add('sun-fill'); // Agregar icono de sol
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        themeIcon.classList.remove('sun-fill'); // Remover icono de sol
+        themeIcon.classList.add('moon-stars-fill'); // Agregar icono de luna
+    }
+}
+
+// Asignar el evento al botón
+document.getElementById('theme-button').addEventListener('click', toggleTheme);
+
