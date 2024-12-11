ceb7f00344b7de3025ef7e96b7844e66230a8305
gabiten <gabiten@gmail.com>
2024-12-11 10:22:50 -0300

    1122332233

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,33 @@
 /**
  * Listen to scroll to change header opacity class
+ * y mostrar/ocultar el botón "Ir arriba"
  */
 function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    var startY = $('.navbar').height() * 2; // El punto donde la navbar cambia en px
 
+    // Cambiar la opacidad de la navbar
     if($(window).scrollTop() > startY){
         $('.navbar').addClass("scrolled");
     }else{
         $('.navbar').removeClass("scrolled");
     }
+
+    // Mostrar el botón de "Ir arriba" si se hace scroll
+    if ($(window).scrollTop() > 100) {
+        $('#scrollToTopBtn').fadeIn();  // Mostrar el botón
+    } else {
+        $('#scrollToTopBtn').fadeOut(); // Ocultar el botón
+    }
 }
+
+// Función para ir arriba
+$('#scrollToTopBtn').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 'smooth');  // Desplazamiento suave
+});
 
 if($('.navbar').length > 0){
     $(window).on("scroll load resize", function(){
         checkScroll();
     });
 }
-
 
